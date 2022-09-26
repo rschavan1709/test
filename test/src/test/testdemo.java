bcd23a480960a23d0a2c4f03b35bcbac2b2028a9
rschavan1709 <94069169+rschavan1709@users.noreply.github.com>
2022-09-26 16:48:23 +0530

    Update testdemo.java

--- test/src/test/testdemo.java
+++ test/src/test/testdemo.java
@@ -4,6 +4,6 @@ public class testdemo {
 	public static void main(String args[]) {
 		System.out.println("Hello world");
 		System.out.println("Riddhi");
-		System.out.println("Riddhi ");
+		System.out.println("Riddhi Chavan ");
 	}
 }
