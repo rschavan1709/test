770845f358f49a6943f7c6167d721a004d7afdde
rschavan1709 <94069169+rschavan1709@users.noreply.github.com>
2022-09-26 16:45:47 +0530

    Update testdemo.java

--- test/src/test/testdemo.java
+++ test/src/test/testdemo.java
@@ -4,6 +4,6 @@ public class testdemo {
 	public static void main(String args[]) {
 		System.out.println("Hello world");
 		System.out.println("Riddhi");
-		System.out.println("Riddhi is pagal");
+		System.out.println("Riddhi ");
 	}
 }
