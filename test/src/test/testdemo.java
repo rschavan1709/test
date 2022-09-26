dbe973261f7b4518e8aa64899151fda35594bebb
kajolmishra948 <60416391+kajolmishra948@users.noreply.github.com>
2022-09-26 16:30:51 +0530

    Update testdemo.java

--- test/src/test/testdemo.java
+++ test/src/test/testdemo.java
@@ -4,5 +4,6 @@ public class testdemo {
 	public static void main(String args[]) {
 		System.out.println("Hello world");
 		System.out.println("Riddhi");
+		System.out.println("Riddhi is pagal");
 	}
 }
